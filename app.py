54af151f8d5d5647176f38e39f61c9ab971679d4
Kiran Solanki <mohcip>
2025-01-14 20:22:24 -0500

    jan 14 home DHruv, made gmail interface better

--- app.py
+++ app.py
@@ -5,37 +5,82 @@ from pygame.locals import QUIT
 
 def scene2(screen, background):
     #background
-    background.fill((255, 255, 255))
+    background.fill((240, 240, 240))
     
-    #Sidebar
-    font = pygame.font.Font('freesansbold.ttf', 28)
-    inbox = font.render('Indox', True, (255,0,0), (255,255,255))
-    starred = font.render('Starred', True, (255,0,0), (255,255,255))
-    important = font.render('Important', True, (255,0,0), (255,255,255))
-    sent = font.render('Sent', True, (255,0,0), (255,255,255))
 
+    #font and size
+    font = pygame.font.Font('C:\Windows\Fonts\Arial.ttf', 28)
+    fontsmaller = pygame.font.Font('C:\Windows\Fonts\Arial.ttf', 22)
 
-   
-    gmaiFull = pygame.image.load("Images/gmailFull.png")
-   
     #main index
+    pygame.draw.rect(background, (255, 255, 255), [220, 75, 1000, 620], 0)
 
     #search bar
     pygame.draw.rect(background, (211, 211, 211), [495, 20, 350, 40], 0)
+    pygame.draw.circle(background, (211,211,211), (490,40), 20)
+    pygame.draw.circle(background, (211,211,211), (850,40), 20)
+    search_logo = pygame.image.load("Images/search_logo.png")
+    Newsearch_logo = pygame.transform.scale(search_logo,(35,32))
+    search_mess = fontsmaller.render('Search mail', True, (0,0,40))
+
+
+    #Sidebar
     
-  
+    fontsmaller = pygame.font.Font('C:\Windows\Fonts\Arial.ttf', 22)
+    inbox = fontsmaller.render('Inbox', True, (0,0,40))
+    starred = fontsmaller.render('Starred', True, (0,0,40))
+    important = fontsmaller.render('Important', True, (0,0,40))
+    sent = fontsmaller.render('Sent', True, (0,0,40))
+    spam = fontsmaller.render('Spam', True, (0,0,40))
+    compose = fontsmaller.render('Compose', True, (0,0,40))
+    pygame.draw.rect(background, (190, 230, 245), [40, 80, 130, 60], 0)
+    pygame.draw.circle(background, (190, 230, 245), (45,110), 30)
+    pygame.draw.circle(background, (190, 230, 245), (170,110), 30)
+
+    pygame.draw.rect(background, (211, 227, 253), [0, 160, 180, 30], 0)
+    pygame.draw.circle(background, (211, 227, 253), (180,175), 15)
+
+    gmaiFull = pygame.image.load("Images/gmailFull.png")
+    back = pygame.image.load("Images/remove.png.png")
+
+    inbox_logo = pygame.image.load("Images/inbox_logo.png")
+    starred_logo = pygame.image.load("Images/starred_logo.png")
+    important_logo = pygame.image.load("Images/important_logo.png")
+    sent_logo = pygame.image.load("Images/sent_logo.png")
+    spam_logo = pygame.image.load("Images/spam_logo.png")
+    compose_logo = pygame.image.load("Images/compose_logo.png")
+    Newcompose_logo = pygame.transform.scale(compose_logo,(38,35))
+
+    Newstarred_logo = pygame.transform.scale(starred_logo,(28,25))
+    Newimportant_logo = pygame.transform.scale(important_logo,(23,21))
+    Newsent_logo = pygame.transform.scale(sent_logo,(30,30))
+    newBack = pygame.transform.scale(back,(100,50))
+    Newspam_logo = pygame.transform.scale(spam_logo,(43,40))
+
     
-    # Rect(left, top, width, height) and pygame.draw.rect(surface, color, rect, width)
+
+    
+    
+    
     
     
     screen.blit(background, (0, 0))
-    screen.blit(inbox, (50, 130))
-    screen.blit(starred, (50, 170))
-    screen.blit(important, (50, 210))
-    screen.blit(sent, (50, 250))
+    screen.blit(inbox, (65, 162))
+    screen.blit(starred, (65, 200))
+    screen.blit(important, (65, 240))
+    screen.blit(sent, (65, 280))
+    screen.blit(spam, (65, 320))
     screen.blit(gmaiFull, (30, 30))
-
-   
+    screen.blit(compose, (74, 96))
+    screen.blit(newBack, (1170, 10))
+    screen.blit(inbox_logo, (20, 163))
+    screen.blit(Newstarred_logo, (16, 200))
+    screen.blit(Newimportant_logo, (19, 240))
+    screen.blit(Newsent_logo, (17, 277))
+    screen.blit(Newspam_logo, (10, 315))
+    screen.blit(Newsearch_logo, (480, 28))
+    screen.blit(search_mess, (530, 28))
+    screen.blit(Newcompose_logo, (25, 92))
    
 
 def scene1(screen, background):
@@ -98,7 +143,8 @@ while True:
     elif pageNumber == 2:
         scene2(screen, background)
         
-        
+
+
     clock.tick(30)
     for event in pygame.event.get():
         if event.type == QUIT:
@@ -112,9 +158,11 @@ while True:
             print(y)
             if x >=549 and x <= 582 and y >=680 and y <=708 and pageNumber == 1:
                 pageNumber = 2
-            elif x >=400 and x <= 500 and y >=0 and y <=100 and pageNumber == 2:
+            elif x >=1242 and x <= 1260 and y >=27 and y <=47 and pageNumber == 2:
                 pageNumber = 1
+
             elif x >=600 and x <=640 and y >=400 and y <=480:
                 print("This is the bottom box")
-                
+
+
     pygame.display.update()
